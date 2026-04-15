362630d574d703d002c4d7307365f7e3dc354c5b
Srimurari14 <murari.dachepalli@gmail.com>
2026-04-15 16:32:39 -0400

    building a random agent

---
+++ src/agent.py
@@ -0,0 +1,50 @@
+from .curling import Curling, SimulationConstants, StoneColor, StoneThrow
+import numpy as np
+
+accurate_constants = SimulationConstants(time_intervals=.05)
+
+def get_state(curling):
+    stone_states = []
+    for i in curling.stones:
+        stone_states.append((i.position, i.color))
+    return stone_states
+
+def max_agent(curling):
+    return StoneThrow(
+            color=my_color,
+            sqrt_velocity=np.random.uniform(1.35, 1.46),
+            angle=np.random.uniform(-.06, .05),
+            spin=np.random.uniform(2., -2.),
+        )
+
+def min_agent(curling):
+    return StoneThrow(
+            color=curling.next_stone_color,
+            sqrt_velocity=np.random.uniform(1.35, 1.46),
+            angle=np.random.uniform(-.06, .05),
+            spin=np.random.uniform(2., -2.),
+        )
+
+my_color = StoneColor.YELLOW
+curling = Curling(my_color)
+curling.reset(starting_color=my_color)
+states = []
+
+
+for i in range(4):
+    if curling.next_stone_color == my_color:
+        action = max_agent(curling)
+    else:
+        action = min_agent(curling)
+
+    curling.throw(action,
+        display=True,
+        constants=accurate_constants
+    )
+
+    state = get_state(curling)
+    states.append(state, i)
+
+score = curling.evaluate_position()
+print(score)
+print(states)
