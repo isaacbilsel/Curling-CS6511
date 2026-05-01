9c2bd9c1703681dd578a4aa6754b1bcc33ce2f66
qj20020222 <jian.qi@gwmail.gwu.edu>
2026-05-01 02:46:12 -0400

    some outline

--- agent/mctsAgent.py
+++ agent/mctsAgent.py
@@ -1,1 +1,42 @@
 from curling import Curling, SimulationConstants, Stone, StoneColor, StoneThrow
+
+class MCTS_Tree_Config:
+    """
+      Iterations number of MCTS
+      number of actions of each dimension in [sqrt_velocity, angle, spin]        
+      ......
+      ......
+
+    """
+
+
+
+class MCTS_Tree_Node:
+    def __init__():
+        "" ""
+
+
+    def node_average_value(self):
+        " "
+    
+
+    def exist_unvisited_node_actions(self):
+        " Whether there are unvisited actions in A(s)"
+
+    def choose_unvisited_node_action(self)->MCTS_Tree_Node:
+        " "
+    
+    def choose_the_best_action(self)->MCTS_Tree_Node:
+        "Use UCB"
+    
+
+
+class MCTS_Agent:
+    def __init__():
+
+    def rollout():
+        """ Play until the end (provided that the node is unvisited)."""
+    
+    def search():
+        "Iteration"
+    
